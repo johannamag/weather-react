7738cd501d71706f72d00b3d02997fb9890f5a5f
johannamag <69107842+johannamag@users.noreply.github.com>
2020-09-10 18:37:18 +0200

    Added github link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <a target="_blank" href="https://github.com/johannamag/weather-react">
+        Github
+      </a>
       <WeatherSearch />
     </div>
   );
